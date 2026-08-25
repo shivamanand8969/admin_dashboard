e925a5a15cac3b03be01dac47195f68e8cbf02bd
shivam anand <shivamanand@shivams-MacBook-Air.local>
2026-08-25 11:56:04 +0530

    initlize the project and set the theme

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function app() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div>
+    Hello
+   </div>
+   </>
+  )
+}
+
+export default app
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,4 @@
+const Topbar=()=>{
+    return <div> TopBar</div>
+}
+export default Topbar;
